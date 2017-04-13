f5af132e05d11082dbe4b0fe9c04d6866c6a7cae
Sean Cali <seankenjicali@gmail.com>
2017-04-13 13:39:36 -0700

    initial commit

---
+++ app/app.module.js
@@ -0,0 +1,52 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app', [])
+        .value('apiUrl', 'http://washmycarapi-dev.azurewebsites.net/api/')
+        .config(function($stateProvider, $urlRouterProvider) {
+            $urlRouterProvider.otherwise('/landing');
+            // Configure each one of our states
+            $stateProvider
+                .state('landing', {
+                    url: '/landing',
+                    controller: '#',
+                    templateUrl: 'app/cmc-marketing/landing.html'
+                })
+                .state('registration', {
+                    url: '/registration',
+                    controller: '#',
+                    templateUrl: 'app/account/registration.html'
+                })
+                .state('search.results', {
+                    url: '/results/:id',
+                    controller: '#',
+                    templateUrl: 'app/cmc-marketing/results.html'
+                })
+                .state('details', {
+                    url: '/detailers/:id',
+                    controller: '#',
+                    templateUrl: 'app/cmc-marketing/details.html'
+                })
+                .state('customer.profile', {
+                    url: '/customers/:id',
+                    controller: '#',
+                    templateUrl: 'app/cmc-marketing/profile.html'
+                })
+                .state('login', {
+                    url: '/login',
+                    controller: '#',
+                    templateUrl: 'app/account/login.html'
+                })
+                .state('appointments', {
+                    url: '/appointments',
+                    controller: '#',
+                    templateUrl: 'app/cmc-detailer/appointments.html'
+                })
+                .state('detailer.profile', {
+                    url: '/detailers/:id',
+                    controller: '#',
+                    templateUrl: 'app/cmc-detailer/profile.html'
+                });
+        });
+})();
