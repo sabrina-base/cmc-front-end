cc726f068e99596ed07abd29aaec268bf7fd3797
Arlin Grijalba <arlin.grijalba@gmail.com>
2017-04-17 20:16:19 -0700

    Map and options list visible with some data of the detailers.

--- app/results/results.controller.js
+++ app/results/results.controller.js
@@ -10,20 +10,44 @@
   /* @ngInject */
   function ResultsController($stateParams, NgMap, $scope, $timeout) {
     var vm = this;
-    vm.resultsTest = "It is working!";
+
+    vm.filterVisibility = "none";
+    vm.priceDivVisibility = false;
+    vm.ratingDivVisibility = false;
+    vm.availabilityDivVisibility = false;
     vm.lat = 32.17;
     vm.long = 55.192;
+
 
     activate();
 
     function activate() {
       loadMap();
-      // vm.options = {
-      //   {name: 'Arlin', rating: 4},
-      //   {name: 'Sabrina', rating: 4.5},
-      //   {name: 'Erin', rating: 4.3},
-      //   {name: 'Eric', rating: 4.2}
-      // };
+      vm.options = [{
+          name: 'Arlin',
+          rating: 4,
+          reviews: 13,
+          multiplier: 0.07
+        },
+        {
+          name: 'Sabrina',
+          rating: 4.5,
+          reviews: 8,
+          multiplier: 0.09
+        },
+        {
+          name: 'Erin',
+          rating: 4.3,
+          reviews: 16,
+          multiplier: 0.15
+        },
+        {
+          name: 'Eric',
+          rating: 4.2,
+          reviews: 11,
+          multiplier: 0.11
+        }
+      ];
     }
 
     function loadMap() {
@@ -44,6 +68,24 @@
           });
         }
       });
+    }
+
+    vm.showPriceDiv = function showPriceDiv() {
+      vm.priceDivVisibility = true;
+      vm.ratingDivVisibility = false;
+      vm.availabilityDivVisibility = false;
+    }
+
+    vm.showRatingDiv = function showRatingDiv() {
+      vm.priceDivVisibility = false;
+      vm.ratingDivVisibility = true;
+      vm.availabilityDivVisibility = false;
+    }
+
+    vm.showAvailabilityDiv = function showAvailabilityDiv() {
+      vm.priceDivVisibility = false;
+      vm.ratingDivVisibility = false;
+      vm.availabilityDivVisibility = true;
     }
   }
 })();
