0d3c6cf9f91dd65b447c585d5de1f2d47e17fbda
Sean Cali <seankenjicali@gmail.com>
2017-04-19 09:55:47 -0700

    added auth factory

---
+++ app/core/authentication.factory.js
@@ -0,0 +1,60 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app.core')
+        .factory('authFactory', authFactory);
+
+    authFactory.$inject = ['apiUrl', '$http', 'localStorageService'];
+
+    /* @ngInject */
+    function authFactory(apiUrl, $http, localStorageService) {
+        var service = {
+          initialize: initialize,
+          logout: logout,
+            register: register,
+            login: login
+            isLoggedIn: false,
+            username: ''
+        };
+
+        return service;
+
+        function register(registrationData) {
+            return $http.post(apiUrl + 'users/register', registrationData)
+        }
+
+        function login(username, password) {
+            var data = 'grant_type=password&' +
+                '&username=' + username +
+                '&password=' + password;
+
+            return $http.post(apiUrl + 'token', data, {
+                    headers: {
+                        'Content-Type': 'application/x-ww-form0urlencoded'
+                    }
+                })
+                .then(function(response) {
+                    localStorageService.set('authorizationData', {
+                        token: reponse.data.access_token,
+                        username: username
+                    });
+                    service.isLoggedIn = true;
+                    service.username = username;
+                    return response.data;
+                });
+        }
+        function initialize() {
+          var authorizationData = localStorageService.get('authorizationData');
+          if(authorizationData) {
+            service.isLoggedIn = true;
+            service.username = authorizationData.username;
+          }
+        }
+        function logout() {
+          localStorageService.remove('authorizationData');
+          service.isLoggedIn = false;
+          service.username = '';
+        }
+    }
+})();
